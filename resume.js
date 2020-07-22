f44c9260462fd5d6cc4c0a09b618d8cb8c04e701
Manfred Cheung <mj3cheun@uwaterloo.ca>
2020-07-22 15:10:44 -0400

    Overhaul resume based on Lauren's advice first draft

--- resume.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('body').html($('body').html().replace(/\r?\n|\r|\t/g, ''));
-	$('.resume-header').each(function(i, obj) {
-		$(this).html($(this).html().replace(/\s/g, '&nbsp;'))
-	});
-
-	$('.resume-header').lettering();
-});
